602c4d6901637428044f617bf9b1ac009bcddc33
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-08-27 14:15:11 +0500

    added description section to cart

--- src/pages/objectCart/ObjectCart.tsx
+++ src/pages/objectCart/ObjectCart.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react'
 // components
 import Header from '../../layouts/header/Header'
 import Slider from './components/slider/Slider'
+import Description from './components/description/Description'
 
 const ObjectCart: FC = () => {
     return (
         <>
             <Header />
             <Slider />
+            <Description />
         </>
     )
 }
